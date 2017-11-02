44a70622e79ff175a44a6aa49e3ff1b6aee37b81
Jacek Styrylski <jacek.styrylski@ac-project.net>
2017-11-02 14:17:47 +0200

    erased old crap

--- 1-Iterations/Program.cs
+++ 1-Iterations/Program.cs
@@ -8,19 +8,9 @@ namespace Iteration
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var solution = new Solution();
+            var sol = new Solution();
             int[] array1 = {1,5,3,3,7};
-            int[] array2 = {1,4,5,2};
-            int[] array3 = {-1,1000000,3647,4};
-            int[] array4 = {1,2,5,4};
-            int[] array5 = {1,5,4,3};
-            int[] array6 = {1,3,5,3,4};
-            Console.WriteLine(solution.solution(array1));
-            Console.WriteLine(solution.solution(array2));
-            Console.WriteLine(solution.solution(array3));
-            Console.WriteLine(solution.solution(array4));
-            Console.WriteLine(solution.solution(array5));
-            Console.WriteLine(solution.solution(array6));
+            Console.WriteLine(sol.Compute(array1));
         }
     }
     
