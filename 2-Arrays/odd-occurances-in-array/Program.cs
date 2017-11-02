2be4dcb6fd9b31a877c69fab7155c5798ca386d0
Jacek Styrylski <jacek.styrylski@gmail.com>
2017-11-02 22:17:25 +0100

    Added odd-occurances-in-array project

---
+++ 2-Arrays/odd-occurances-in-array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine("Hello World!\n\n");
+            Console.WriteLine(sol.solution(1));
+            Console.WriteLine(sol.solution(9));
+            Console.WriteLine(sol.solution(17));
+            Console.WriteLine(sol.solution(65));
+            Console.WriteLine(sol.solution(2147483647));
+        }
+    }
+    
+}
---
+++ 2-Arrays/odd-occurances-in-array/Solution.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+
+namespace Iteration
+{
+    class Solution {
+        private int value;
+        private string binaryValue;
+        private int[] binaryArray;
+        public int solution(int n) {
+            value = n;
+            AssertCorrectValues();
+            ConvertToBinary();
+            return ComputeBinaryGap();
+        }
+
+        private int ComputeBinaryGap()
+        {
+            int counter = 0;
+            int maxSize = 0;
+            foreach (var bit in binaryArray)
+            {
+                if(bit == 0) counter++;
+                else 
+                {
+                    if(counter > maxSize) maxSize = counter;
+                    counter = 0;
+                }
+            }
+            return maxSize;
+        }
+
+        private void ConvertToBinary()
+        {
+            binaryValue = Convert.ToString(value, 2); 
+            binaryArray = binaryValue.Select(x => int.Parse(x.ToString())).ToArray();
+        }
+        private void AssertCorrectValues()
+        {
+            const string NOT_SUPPORT_VALUE = 
+                @"This algorithm do not support 
+                computation of values lower than 1";
+
+            if(value < 1) 
+                throw new NotSupportedException(NOT_SUPPORT_VALUE);
+        }
+    }
+}
