6564e0d83c8317861bc2cb8c2ee95569afe26adc
Jacek Styrylski <jacek.styrylski@ac-project.net>
2017-11-02 15:18:42 +0200

    finished task

--- 1-Iterations/Program.cs
+++ 1-Iterations/Program.cs
@@ -7,10 +7,13 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var sol = new Solution();
-            int[] array1 = {1,5,3,3,7};
-            Console.WriteLine(sol.Compute(array1));
+            Console.WriteLine("Hello World!\n\n");
+            Console.WriteLine(sol.solution(1));
+            Console.WriteLine(sol.solution(9));
+            Console.WriteLine(sol.solution(17));
+            Console.WriteLine(sol.solution(65));
+            Console.WriteLine(sol.solution(2147483647));
         }
     }
     
