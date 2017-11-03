e9e5deb8f3df232f3706c3dd1663b90885e625eb
Jacek Styrylski <jacek.styrylski@gmail.com>
2017-11-03 13:16:57 +0100

    Cyclic rotatio

---
+++ 2-Arrays/cyclick-rotation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine("Hello World!\n\n");
+            Console.WriteLine(sol.solution(new int[]{2,3,5,3,2}, 1));
+            Console.WriteLine(sol.solution(new int[]{4,5,4,5,5,5,2,3,5,3,2},3));
+        }
+    }
+    
+}
---
+++ 2-Arrays/cyclick-rotation/Solution.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iteration
+{
+    class Solution {
+        private int[] elements;
+        private int numberOfRotations;
+        public int[] solution(int[] A, int K) {
+            elements = A;
+            numberOfRotations = K;
+            RotateElements();
+            return elements;
+        }
+
+        private void RotateElements()
+        {
+            if(numberOfRotations == 0 || elements.Count() < 2)
+                return;
+                
+            for (int i = 0; i < numberOfRotations; i++)
+            {
+                SingleRotate();
+            }
+        }
+
+        private void SingleRotate()
+        {
+            int temp = elements[elements.Count() - 1];
+            for (int i = elements.Count() - 2; i > -1; i--)
+            {
+                elements[i+1] = elements[i];
+            }
+            elements[0] = temp;
+        }
+
+    }
+}
